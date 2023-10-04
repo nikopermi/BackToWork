4def161928810a8215e865fb470daa79de868d2c
Niko <nikopermilovsky@gmail.com>
2023-10-03 20:32:56 -0700

    init

---
+++ src/main/kotlin/com/example/backtowork/BackToWorkApplication.kt
@@ -0,0 +1,11 @@
+package com.example.backtowork
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackToWorkApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackToWorkApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/backtowork/BackToWorkApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.backtowork
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackToWorkApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
